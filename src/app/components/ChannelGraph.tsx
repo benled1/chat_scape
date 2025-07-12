1d4d4b80080d0390c6d3c35c33cac2bc079b5721
Ben Ledingham <benledingham1@gmail.com>
2025-07-12 15:55:17 -0700

    cors issue, but done for now

--- src/app/components/ChannelGraph.tsx
+++ src/app/components/ChannelGraph.tsx
@@ -1,6 +1,16 @@
 'use client'
+import dynamic from 'next/dynamic';
 import React from 'react';
-import ForceGraph3D, {ForceGraphMethods, NodeObject, LinkObject} from 'react-force-graph-3d';
+import type {
+    ForceGraphMethods,
+    NodeObject,
+    LinkObject,
+} from 'react-force-graph-3d';
+const ForceGraph3D = dynamic(
+  () => import('react-force-graph-3d').then((mod) => mod.default),
+  { ssr: false }
+);
+
 interface GraphNode extends NodeObject {
     id: string;
     name: string;
@@ -12,22 +22,29 @@ interface GraphLink extends LinkObject {
     target: string;
 }
 
+
+const dummyGraphData: { nodes: GraphNode[], links: GraphLink[]} = {
+    nodes: [
+        {id:'id1', name: 'name1', val: 1},
+        {id:'id2', name: 'name2', val: 2},
+        {id:'id3', name: 'name3', val: 3}
+    ],
+    links: [
+        {source: 'id1', target: 'id2'},
+        {source: 'id2', target: 'id3'}
+    ],
+};
+
 export default function ChannelGraph() {
-    const graphData: { nodes: GraphNode[], links: GraphLink[]} = {
-        nodes: [
-            {id:'id1', name: 'name1', val: 1},
-            {id:'id2', name: 'name2', val: 2},
-            {id:'id3', name: 'name3', val: 3}
-        ],
-        links: [
-            {source: 'id1', target: 'id2'},
-            {source: 'id2', target: 'id3'}
-        ],
-    };
+    // retrieve channel coord data
+    const response = fetch("http://localhost:8000/channels");
+    console.log(`The response = ${response}`);
+
+
     return (
         <div style={{width: '600px', height: '600px'}}>
             <ForceGraph3D
-                graphData={graphData}
+                graphData={dummyGraphData}
                 nodeRelSize={8}
                 nodeLabel="name"
                 linkDirectionalParticles={2}
--- src/app/components/hello-world.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Hello(){
-    return(
-        <h1>Hello</h1>
-    );
-};
